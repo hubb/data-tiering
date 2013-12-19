702884bce748a64b3ca6a6fa684f12103ab24e67
Daniel Grieve <daniel.grieve@neo.com>
2013-12-19 14:47:10 +0000

    move to expect syntax. remove silly constants

--- lib/data_tiering/configuration.rb
+++ lib/data_tiering/configuration.rb
@@ -1,15 +1,11 @@
 module DataTiering
   class Configuration
-    attr_accessor :search_enabled, :sync_enabled, :models_to_sync
-
-    DEFAULT_SEARCH_ENABLED = true
-    DEFAULT_SYNC_ENABLED   = true
-    DEFAULT_MODELS_TO_SYNC = []
+    attr_accessor :models_to_sync, :search_enabled, :sync_enabled
 
     def initialize
-      @search_enabled = DEFAULT_SEARCH_ENABLED
-      @sync_enabled   = DEFAULT_SYNC_ENABLED
-      @models_to_sync = DEFAULT_MODELS_TO_SYNC
+      @models_to_sync = []
+      @search_enabled = true
+      @sync_enabled   = true
     end
   end
 end
--- spec/lib/data_tiering/configuration_spec.rb
+++ spec/lib/data_tiering/configuration_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe DataTiering::Configuration do
   it 'provides default values for configuration options' do
-    subject.search_enabled.should be_true
-    subject.sync_enabled.should be_true
-    subject.models_to_sync.should == []
+    expect(subject.search_enabled).to be_true
+    expect(subject.sync_enabled).to be_true
+    expect(subject.models_to_sync).to eq([])
   end
 
   it 'allows configuration options to be changed' do
     subject.search_enabled = false
-    subject.search_enabled.should be_false
+    expect(subject.search_enabled).to be_false
 
     subject.sync_enabled = false
-    subject.sync_enabled.should be_false
+    expect(subject.sync_enabled).to be_false
 
     subject.models_to_sync = [1,2,3]
-    subject.models_to_sync.should == [1,2,3]
+    expect(subject.models_to_sync).to eq([1,2,3])
   end
 end
