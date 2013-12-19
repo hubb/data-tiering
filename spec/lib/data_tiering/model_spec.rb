b6e2e959c1b5755fb31e73e8447c97f02a6d1460
Thibault Gautriaud <tgautriaud@housetrip.com>
2013-12-19 09:59:08 +0000

    Fixes model spec

--- spec/lib/data_tiering/model_spec.rb
+++ spec/lib/data_tiering/model_spec.rb
@@ -4,24 +4,35 @@ shared_examples_for "a model that is part of data tiering" do
 
   describe '#row_touched_at' do
 
-    it 'complains when being read' do
-      proc do
+    let(:time) { Time.parse("12pm 1st January 2013") }
+
+    it 'raises an error when being read' do
+      expect {
         described_class.new.row_touched_at
-      end.should raise_error("this is a MySQL timestamp, don't use it as an AR attribute")
+      }.to raise_error("this is a MySQL timestamp, don't use it as an AR attribute")
     end
 
-    it 'does not allow accidental overridding via mass assignment' do
-      t = Time.parse("12pm 1st January 2013")
-      model = described_class.create!(:row_touched_at => t)
-      model.attributes = { :row_touched_at => t + 1.year }
-      model.read_attribute(:row_touched_at).should == t
+    it 'allows direct attribute assignment on row_touched_at' do
+      subject.row_touched_at = time
+      subject.save!
+
+      expect(subject.read_attribute(:row_touched_at)).to eql(time)
+    end
+
+    it 'does not allow mass assignment on creation' do
+      instance = described_class.create!(:row_touched_at => time)
+      instance.read_attribute(:row_touched_at).should be_nil
+    end
+
+    it 'does not allow mass assignment on attributes change' do
+      subject.row_touched_at = time
+      subject.attributes = { :row_touched_at => time + 1.year }
+      subject.read_attribute(:row_touched_at).should == time
     end
 
   end
 
 end
-
-class Property < ::ActiveRecord::Base; end
 
 describe Property do
 
