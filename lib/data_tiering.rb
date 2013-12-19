8e7816048802773576a5a7d9daf242bb47b41611
Daniel Grieve <daniel.grieve@neo.com>
2013-12-19 14:55:30 +0000

    add configuration to the DataTiering module

--- lib/data_tiering.rb
+++ lib/data_tiering.rb
@@ -3,8 +3,18 @@
 lib = File.expand_path('lib')
 $:.unshift(lib) unless $:.include?(lib)
 
+require 'data_tiering/configuration'
+
 module DataTiering
   VERSION = '0.0.1'
+
+  def self.configure
+    yield(configuration)
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
 end
 
 # require 'data_tiering/sync'
