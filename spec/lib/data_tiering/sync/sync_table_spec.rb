3e14c07c155e9ac08696d65867b59463609a3b2c
Thibault Gautriaud <tgautriaud@housetrip.com>
2013-12-20 13:12:49 +0000

    WIP - Fixing sync_table_spec

--- spec/lib/data_tiering/sync/sync_table_spec.rb
+++ spec/lib/data_tiering/sync/sync_table_spec.rb
@@ -3,17 +3,20 @@ require 'data_tiering/sync/sync_table'
 
 shared_examples_for 'a basic table sync mechanism' do
 
+  before do
+    Property.create!(:name => "property 1")
+    Property.create!(:name => "property 2")
+  end
+
   it 'copies over all properties' do
-    DataTieringSyncSpec::Property.create!(:name => "property 1")
-    DataTieringSyncSpec::Property.create!(:name => "property 2")
-    sync
+    subject.sync
+
     DataTieringSyncSpec::Property.all.collect(&:name).should =~ ["property 1", "property 2"]
   end
 
   it 'does not change ids' do
-    DataTieringSyncSpec::Property.create!
-    DataTieringSyncSpec::Property.create!
-    sync
+    subject.sync
+
     DataTieringSyncSpec::Property.all.collect(&:id).should =~ Property.all.collect(&:id)
   end
 
@@ -21,87 +24,65 @@ end
 
 
 describe DataTiering::Sync::SyncTable do
-  before do
-    Rails = double(:env => double(:test? => true))
-  end
-
-
-  def build_sync_table
-    DataTiering::Sync::SyncTable.new("properties", "properties_secondary_0")
-  end
+  subject { described_class.new("properties", "properties_secondary_0") }
 
   describe '#delta_sync_possible?' do
-
     it 'is true if there is a secondary table with the same schema' do
-      build_sync_table.send(:recreate_inactive_table)
+      subject.send(:recreate_inactive_table)
       DataTieringSyncSpec::Property.reset_column_information
-      build_sync_table.send(:delta_sync_possible?).should be_true
+      subject.send(:delta_sync_possible?).should be_true
     end
 
     it 'is false if the secondary table is missing' do
-      build_sync_table.send(:drop_inactive_table)
-      build_sync_table.send(:delta_sync_possible?).should be_false
+      subject.send(:drop_inactive_table)
+      subject.send(:delta_sync_possible?).should be_false
     end
 
     it 'is false if the secondary tables schema is different' do
-      build_sync_table.send(:recreate_inactive_table)
+      subject.send(:recreate_inactive_table)
       DataTieringSyncSpec::Property.reset_column_information
       ActiveRecord::Base.connection.remove_column "properties_secondary_0", "name"
-      build_sync_table.send(:delta_sync_possible?).should be_false
+      subject.send(:delta_sync_possible?).should be_false
     end
-
   end
 
   describe '#sync' do
 
-    context 'when using complete syncing' do
-
+    context 'when delta syncing is not possible' do
       before do
-        build_sync_table.send(:drop_inactive_table)
-      end
-
-      def sync
-        sync_table = build_sync_table
-        sync_table.stub :delta_sync_possible? => false
-        sync_table.sync
+        subject.stub :delta_sync_possible? => false
       end
 
       it 'calls sync_all' do
-        sync_table = build_sync_table
-        sync_table.should_receive(:sync_all)
-        sync_table.sync
+        subject.should_receive(:sync_all)
+        subject.sync
       end
 
       it_should_behave_like 'a basic table sync mechanism'
 
       it 'creates secondary table with the correct columns' do
+        subject.send(:recreate_inactive_table)
+        subject.sync
         DataTieringSyncSpec::Property.columns.collect(&:name).should == Property.columns.collect(&:name)
       end
 
     end
 
-    context 'when using delta syncing' do
+    context 'when delta syncing is possible' do
 
-      before(:all) do
-        build_sync_table.send(:recreate_inactive_table)
-      end
-
-      def sync
-        sync_table = build_sync_table
-        sync_table.stub :delta_sync_possible? => true
-        sync_table.sync
+      before do
+        subject.send(:recreate_inactive_table)
+        subject.stub(:delta_sync_possible? => true)
       end
 
       it 'calls sync_deltas' do
-        sync = DataTiering::Sync::SyncTable.new("properties", "properties_secondary_0")
-        sync.should_receive(:sync_deltas)
-        sync.sync
+        subject.should_receive(:sync_deltas)
+        subject.sync
       end
 
       it 'sets the db isolation level to "READ COMMITTED"' do
-        sync = DataTiering::Sync::SyncTable.new("properties", "properties_secondary_0")
-        sync.should_receive(:with_transaction_isolation_level).with("READ COMMITTED")
-        sync.sync
+        subject.should_receive(:with_transaction_isolation_level).with("READ COMMITTED")
+        subject.sync
       end
 
       it_should_behave_like 'a basic table sync mechanism'
@@ -113,100 +94,109 @@ describe DataTiering::Sync::SyncTable do
 
       it 'syncs changes close to the last seen change' do
         property = Property.create!(:name => "old name")
-        sync
-        Timecop.freeze(5.minutes.ago) do
+        subject.sync
+        DataTieringSyncSpec::Property.first.name.should == "old name"
+
+        Timecop.freeze(Time.current - 5.minutes) do
           property.name = "new name"
           property.save(false)
           set_row_touched_at(property)
         end
-        sync
+        subject.sync
         DataTieringSyncSpec::Property.first.name.should == "new name"
       end
 
       it 'does not sync changes that happened a while before the last seen change' do
         property = Property.create!(:name => "old name")
-        sync
-        Timecop.freeze(60.minutes.ago) do
+        subject.sync
+        Timecop.freeze(Time.current - 60.minutes) do
           property.name = "new name"
           property.save(false)
           set_row_touched_at(property)
         end
-        sync
+        subject.sync
         DataTieringSyncSpec::Property.first.name.should == "old name"
       end
 
       it 'syncs new properties' do
-        DataTieringSyncSpec::Property.create!(:name => "property 1")
-        sync
-        DataTieringSyncSpec::Property.create!(:name => "property 2")
-        sync
+        Property.create!(:name => "property 1")
+        subject.sync
+        Property.create!(:name => "property 2")
+        subject.sync
         DataTieringSyncSpec::Property.count.should == 2
       end
 
       it 'syncs changed properties' do
         property = Property.create!(:name => "old name")
-        sync
+        subject.sync
         property.name = "new name"
         property.save(false)
-        sync
+        subject.sync
         DataTieringSyncSpec::Property.first.name.should == "new name"
       end
 
       it 'syncs deletions, but does not touch the regular table' do
-        DataTieringSyncSpec::Property.create!(:name => "property 1")
-        property = DataTieringSyncSpec::Property.create!(:name => "property 2")
-        sync
+        Property.create!(:name => "property 1")
+        property = Property.create!(:name => "property 2")
+        subject.sync
+
         property.delete
-        sync
-        DataTieringSyncSpec::Property.count.should == 1
-        DataTieringSyncSpec::Property.count.should == 1
+
+        expect { subject.sync }.to change(DataTieringSyncSpec::Property, :count).by(-1)
       end
 
       context "batch processing" do
 
+        let!(:property1) { Property.create!(:name => "Hello") }
+        let!(:property2) { Property.create!(:name => "World!") }
+
         before do
           @original_contstant_value = DataTiering::Sync::SyncTable::SyncDeltas::BATCH_SIZE
-          DataTiering::Sync::SyncTable::SyncDeltas::BATCH_SIZE = 1000
-          DataTieringSyncSpec::Property.create!(:id => 100)
-          DataTieringSyncSpec::Property.create!(:id => 1100)
+          DataTiering::Sync::SyncTable::SyncDeltas::BATCH_SIZE = 1
         end
 
         after do
           DataTiering::Sync::SyncTable::SyncDeltas::BATCH_SIZE = @original_contstant_value
         end
 
-        it 'copies all properties when there is more than one batch' do
-          sync
-          DataTieringSyncSpec::Property.all.collect(&:id).should =~ [100, 1100]
-        end
+        context 'when more than one batch' do
+          before { subject.sync }
 
-        it 'deletes properties when there is more than one batch' do
-          sync
-          DataTieringSyncSpec::Property.find(1100).delete
-          sync
-          DataTieringSyncSpec::Property.all.collect(&:id).should =~ [100]
+          it 'copies all properties' do
+            DataTieringSyncSpec::Property.all.collect(&:id).should =~ [property1.id, property2.id]
+          end
+
+          it 'deletes properties' do
+            Property.find(property2.id).delete
+            subject.sync
+
+            DataTieringSyncSpec::Property.all.collect(&:id).should =~ [property1.id]
+          end
         end
 
         it 'only takes timestamps from an already processed batch' do
           Timecop.freeze(1.month.from_now) do
-            DataTieringSyncSpec::Property.find(100).save(false)
+            Property.find(property1.id).save(false)
           end
-          sync
-          DataTieringSyncSpec::Property.all.collect(&:id).should =~ [100, 1100]
+
+          subject.sync
+          DataTieringSyncSpec::Property.all.collect(&:id).should =~ [property1.id, property2.id]
         end
 
         it 'performs only one insert per batch' do
           DataTiering::Sync::SyncLog.stub(:log).and_yield
+
           ActiveRecord::Base.connection.should_receive(:insert).exactly(:twice)
-          sync
+          subject.sync
         end
 
-        it 'performes only one delete per batch' do
-          sync
-          DataTieringSyncSpec::Property.delete_all
+        it 'performs only one delete per batch' do
+          subject.sync
+          Property.delete_all
           DataTiering::Sync::SyncLog.stub(:log).and_yield
+
           ActiveRecord::Base.connection.should_receive(:delete).exactly(:twice)
-          sync
+          subject.sync
         end
 
       end
@@ -216,8 +206,6 @@ describe DataTiering::Sync::SyncTable do
   end
 
   describe '#with_transaction_isolation_level' do
-
-    subject { build_sync_table }
 
     def current_transaction_isolation_level
       ActiveRecord::Base.connection.select_value(<<-SQL).sub('-', ' ')
